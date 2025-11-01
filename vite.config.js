96b7e8f00fd4092ae3b337157c91897f3f42fa21
nicoi <nico.imesch@outlook.com>
2025-11-01 15:50:26 +0100

    Vite.config.js angepasst und npm run build gemacht, um ein Jar für den ersten Test zu erstellen

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // <-- sehr wichtig für Spring Boot
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       "/api": {
